734cfa30207f6dd42932e4f22a334a95f564b86e
Pats-lang <128126754+Pats-lang@users.noreply.github.com>
2023-12-18 19:05:53 +0800

    d

--- app/src/main/java/com/example/barangay20/Certif_ScrollingFragment.kt
+++ app/src/main/java/com/example/barangay20/Certif_ScrollingFragment.kt
@@ -1,113 +1,78 @@
 package com.example.barangay20
 
-import android.annotation.SuppressLint
-import android.app.AlertDialog
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.android.volley.Request
-import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.google.android.material.textfield.TextInputEditText
-import org.json.JSONObject
-
+import java.util.HashMap
 
 class Certif_ScrollingFragment : Fragment() {
-    private fun showSuccessAlertDialog() {
-        val alertDialogBuilder = AlertDialog.Builder(requireContext())
-
-        // Set the title and message for success
-        alertDialogBuilder.setTitle("Success")
-        alertDialogBuilder.setMessage("Data added successfully!")
-
-        // Set the positive button
-        alertDialogBuilder.setPositiveButton("OK") { dialog, which ->
-            // Navigate to the next page
-            val registerIntent = Intent(requireContext(), RequestFragment::class.java)
-            startActivity(registerIntent)
-
-            // Finish this fragment to prevent going back to it
-            requireActivity().supportFragmentManager.beginTransaction().remove(this).commit()
-
-
-            dialog.dismiss()
-        }
-
-        // Create and show the AlertDialog
-        val alertDialog: AlertDialog = alertDialogBuilder.create()
-        alertDialog.show()
-    }
-    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var barangayclearance = inflater.inflate(R.layout.fragment_certif__scrolling, container, false)
-        val textcertiname: TextInputEditText =
-            barangayclearance.findViewById(R.id.textcertiname)
-        val textcertiAddress: TextInputEditText =
-            barangayclearance.findViewById(R.id.textcertiAddress)
-        val textcertiRes: TextInputEditText =
-            barangayclearance.findViewById(R.id.textcertiRes)
-        val textcertiContact: TextInputEditText =
-            barangayclearance.findViewById(R.id.textcertiContact)
-        val textcertiPurpose: TextInputEditText =
-            barangayclearance.findViewById(R.id.textcertiPurpose)
-        val textCertiRequest: TextInputEditText =
-            barangayclearance.findViewById(R.id.textCertiRequest)
+
+        val view = inflater.inflate(R.layout.fragment_certif__scrolling, container, false)
+        val submit = view.findViewById<Button>(R.id.submit)
+        submit.setOnClickListener {
+            requestForm()
+        }
+        return view
+    }
 
 
+    private fun requestForm() {
+        val certiNameEditText: TextInputEditText = requireView().findViewById(R.id.textcertiname)
+        val certiAddressEditText: TextInputEditText = requireView().findViewById(R.id.textcertiAddress)
+        val certiyearresEditText: TextInputEditText = requireView().findViewById(R.id.textcertiRes)
+        val certicontactEditText: TextInputEditText = requireView().findViewById(R.id.textcertiContact)
+        val certiemailEditText: TextInputEditText = requireView().findViewById(R.id.CertiEmail)
+        val certipurposeEditText: TextInputEditText = requireView().findViewById(R.id.textcertiPurpose)
+        val certirequestEditText: TextInputEditText = requireView().findViewById(R.id.textCertiRequest)
 
+        val name = certiNameEditText.text.toString().trim()
+        val address = certiAddressEditText.text.toString().trim()
+        val year_res = certiyearresEditText.text.toString().trim()
+        val contact = certicontactEditText.text.toString().trim()
+        val email = certiemailEditText.text.toString().trim()
+        val purpose = certipurposeEditText.text.toString().trim()
+        val requested = certirequestEditText.text.toString().trim()
 
-        val submitButton: Button = barangayclearance.findViewById(R.id.submit)
-        // Set OnClickListener for the Button
-        submitButton.setOnClickListener {
-            val name = textcertiname.text.toString()
-            val address = textcertiAddress.text.toString()
-            val yrs_res = textcertiRes.text.toString()
-            val contact = textcertiContact.text.toString()
-            val purpose = textcertiPurpose.text.toString()
-            val Request =  textCertiRequest.text.toString()
-
-            // Make a Volley request
-            val url = "https://barangaymngmt.bsisakalam.com/barangay_clearance.php"
-            val params = HashMap<String, String>()
-            params["name"] = name
-            params["address"] = address
-            params["yrs_res"] = yrs_res
-            params["contact_no"] = contact
-            params["purpose"] = purpose
-            params["request"] = Request
-            val jsonObjectRequest = JsonObjectRequest(
-                com.android.volley.Request.Method.POST, url, (params as Map<*, *>?)?.let { it1 -> JSONObject(it1) },
-                { response ->
-                    val status = response.optBoolean("status")
-                    val message = response.optString("message")
-
-                    if (status) {
-                        Toast.makeText(requireContext(), "Added successfully: $message", Toast.LENGTH_SHORT).show()
-                        showSuccessAlertDialog()
-                    } else {
-                        Toast.makeText(requireContext(), "Failed to add: $message", Toast.LENGTH_SHORT).show()
-                    }
-                },
-                { error ->
-                    Toast.makeText(requireContext(), "Request error: ${error.localizedMessage}", Toast.LENGTH_SHORT).show()
+        val url = "http://192.168.125.49/api/req_brgyclrs_api.php"
+        val queue = Volley.newRequestQueue(requireContext())
+        val request = object : StringRequest(Request.Method.POST, url,
+            { response ->
+                try {
+                    Toast.makeText(requireContext(), "$response", Toast.LENGTH_LONG).show()
+                } catch (e: Exception) {
+                    Toast.makeText(requireContext(), "Request Failed", Toast.LENGTH_LONG).show()
                 }
-            )
-            Volley.newRequestQueue(requireContext()).add(jsonObjectRequest)
+            },
+            { error ->
+                Toast.makeText(requireContext(), "Request Error", Toast.LENGTH_LONG).show()
+            }) {
+
+            override fun getParams(): MutableMap<String, String> {
+                val params = HashMap<String, String>()
+                params["name"] = name
+                params["address"] = address // Change this to the appropriate field name
+                params["yrs_res"] = year_res
+                params["contact"] = contact
+                params["email"] = email
+                params["purpose"] = purpose
+                params["request"] = requested
+                return params
+            }
         }
 
-
-        return barangayclearance
+        queue.add(request)
     }
 }
